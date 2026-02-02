688b64b3d14ef4a58a2447695d849d38f799e272
Ciel806 <ceryne688@gmail.com>
2026-02-02 16:12:53 +0700

    week01: refactor to use val and string templates

--- src/oop_00000135652_Ceryne/week01/GradingSystem.kt
+++ src/oop_00000135652_Ceryne/week01/GradingSystem.kt
@@ -1,4 +1,10 @@
 package oop_00000135652_Ceryne.week01
 
-class GradingSystem {
+fun main() {
+    // REFACTOR: gunakan val dan hapus tipe data eksplisit
+    val name: String = "John Thor"
+    val score: Int = 80
+
+    //refactor: String template ($name)
+    println("Nama: $name, score: $score")
 }
