d3f65471b92ecd4e9889f5bf10ec7d45d24cc916
Ciel806 <ceryne688@gmail.com>
2026-02-16 21:41:09 +0700

    week03: (task 2) implement Player testing in main

---
+++ src/oop_00000135652_Ceryne/week03/task02/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000135652_Ceryne.week03.task02
+
+fun main() {
+    val p = Player("Zero")
+
+    p.addXp(50)
+    println("${p.username} is on Level ${p.level}")
+
+    p.addXp(60)
+}
