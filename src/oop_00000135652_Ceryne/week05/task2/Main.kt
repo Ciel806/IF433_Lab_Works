6039702bb947175a03a45fa011be0f8f98b532dd
Ciel806 <ceryne688@gmail.com>
2026-03-02 15:50:42 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_00000135652_Ceryne/week05/task2/Main.kt
@@ -0,0 +1,17 @@
+package oop_00000135652_Ceryne.week05.task2
+
+import oop_00000135652_Ceryne.week05.Admin
+import oop_00000135652_Ceryne.week05.Dosen
+
+fun main() {
+    val wallet = EWallet("Budi", 50000.0)
+    val credit = CreditCard("Budi", 1000000.0)
+
+    val paymentList: List<PaymentMethod> = listOf(wallet, credit)
+
+    println("=== TRANSACTION ACTIVITY ===")
+    for (method in paymentList) {
+        method.processPayment(75000.0)
+
+    }
+}
