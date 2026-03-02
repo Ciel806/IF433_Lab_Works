c64f18203ea0302a86bfd8d9d8870ae913c0ae2f
Ciel806 <ceryne688@gmail.com>
2026-03-02 15:09:32 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000135652_Ceryne/week05/task1/MathHelper.kt
@@ -0,0 +1,15 @@
+package oop_00000135652_Ceryne.week05.task1
+
+class MathHelper {
+    fun hitungLuas (sisi: Int) : Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas (jarijari: Double) : Double {
+        return 3.14 * jarijari * jarijari
+    }
+}
