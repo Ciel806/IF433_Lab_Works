bbc4f332a46eced2908255b7a6bd6c7d601fe4ae
Ciel806 <ceryne688@gmail.com>
2026-03-09 20:31:36 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000135652_Ceryne/week06/task/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000135652_Ceryne.week06.task
+
+fun main () {
+    val Lamp = SmartLamp("1", "Ruang Tamu")
+    val Speaker = SmartSpeaker("1", "Google Nest Dapur")
+    val CCTV = SmartCCTV("1", "Ezviz Garasi")
+}
