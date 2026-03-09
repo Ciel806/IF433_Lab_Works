20afa89790381b067e70e4512485c0b1fd45716f
Ciel806 <ceryne688@gmail.com>
2026-03-09 16:14:16 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_00000135652_Ceryne/week06/task/SmartInterfaces.kt
@@ -0,0 +1,18 @@
+package oop_00000135652_Ceryne.week06.task
+
+interface SmartInterfaces {
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
