6d9018ec9de7b97ca5b18bff1a0f1ad334c2ef58
Ciel806 <ceryne688@gmail.com>
2026-03-09 13:56:05 +0700

    week06: create Gopay and creditCard

--- src/oop_00000135652_Ceryne/week06/PaymentMethod.kt
+++ src/oop_00000135652_Ceryne/week06/PaymentMethod.kt
@@ -1,5 +1,19 @@
 package oop_00000135652_Ceryne.week06
 
+import kotlin.math.tan
+
 interface PaymentMethod {
     fun pay(amount: Double)
+}
+
+class Gopay : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Processing Rp$amount via Gopay Server")
+    }
+}
+
+class CreditCard : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Contacting Bank for Rp$amount")
+    }
 }
