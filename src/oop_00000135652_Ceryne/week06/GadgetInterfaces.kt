26853d1df078c830bdb8a90ede2981c8c04038a7
Ciel806 <ceryne688@gmail.com>
2026-03-09 13:45:34 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000135652_Ceryne/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000135652_Ceryne.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
