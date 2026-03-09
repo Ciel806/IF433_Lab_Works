38ed5aa2c029e9723fd5837e70c6d74f5a6168a7
Ciel806 <ceryne688@gmail.com>
2026-03-09 20:22:34 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000135652_Ceryne/week06/task/SmartHomeHub.kt
@@ -0,0 +1,21 @@
+package oop_00000135652_Ceryne.week06.task
+
+class SmartHomeHub () {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    //Buat fungsi turnOffAllSwitches(). Lakukan iterasi for ke devices. Gunakan
+    //Smart Casting (is) untuk mendeteksi: Jika device tersebut adalah sebuah
+    //Switchable, panggil turnOff().
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
+}
