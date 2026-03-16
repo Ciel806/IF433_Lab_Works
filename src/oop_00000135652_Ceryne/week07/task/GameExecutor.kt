d610beb390aa8b38808af4eabb2746b62cc8b413
Ciel806 <ceryne688@gmail.com>
2026-03-16 20:42:43 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_00000135652_Ceryne/week07/task/GameExecutor.kt
+++ src/oop_00000135652_Ceryne/week07/task/GameExecutor.kt
@@ -3,23 +3,27 @@ package oop_00000135652_Ceryne.week07.task
 fun processEvent(event: BattleState) {
     when (event) {
         is BattleState.MonsterEncounter -> {
-            println("You've encountered a Monster!")
+            println("You've encountered ${event.monsterName}!")
+            println()
         }
 
         is BattleState.LootDropped -> {
             println("You've obtained a loot!")
-            println("\nName: ${event.item.name}")
-            println("\nDamage: ${event.item.damage}")
-            println("\nRarity: ${event.item.rarity}")
+            println("Name: ${event.item.name}")
+            println("Damage: ${event.item.damage}")
+            println("Rarity: ${event.item.rarity}")
+            println()
         }
 
         is BattleState.GameOver -> {
             println("GAME OVER!")
             println("Reason: ${event.reason}")
+            println()
         }
 
         is BattleState.SafeZone -> {
             println("You've entered a SAFE ZONE!")
+            println()
         }
     }
 }
--- src/oop_00000135652_Ceryne/week07/task/Main.kt
+++ src/oop_00000135652_Ceryne/week07/task/Main.kt
@@ -4,7 +4,7 @@ fun main() {
     GameManager.startGame()
     GameManager.startGame()
 
-    println("LEGENDARY Weapon drop chance: ${ItemRarity.LEGENDARY.dropChance}%")
+    println("LEGENDARY Weapon drop chance: ${ItemRarity.LEGENDARY.dropChance}%\n")
 
     val starterSword = Weapon.forgeStarterSword()
 
@@ -13,4 +13,13 @@ fun main() {
     println("Damage: ${starterSword.item.damage}")
     println("Rarity: ${starterSword.item.rarity}")
     println()
+
+    val upgradedItem = starterSword.item.copy(
+        damage = 25
+    )
+
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgradedItem))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
