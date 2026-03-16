ae81a4725f9f49f9f3f84443be1019207c4c2716
Ciel806 <ceryne688@gmail.com>
2026-03-16 13:53:05 +0700

    week07: (trial) domonstrate regular class equality failure

--- src/oop_00000135652_Ceryne/week07/Main.kt
+++ src/oop_00000135652_Ceryne/week07/Main.kt
@@ -8,4 +8,10 @@ fun main() {
     println("\n=== TEST COMPANION OBJECT ===")
     val client = NetworkClient.createClient() //instansi lewat factory
     client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegulerUser("Alice", 22)
+    val reg2 = RegulerUser("Alice", 22)
+    println(reg1) //cetak memori hash
+    println("Sama? ${reg1 == reg2}") // false
 }
---
+++ src/oop_00000135652_Ceryne/week07/UserModels.kt
@@ -0,0 +1,3 @@
+package oop_00000135652_Ceryne.week07
+
+class RegulerUser(val name: String, val age: Int)
