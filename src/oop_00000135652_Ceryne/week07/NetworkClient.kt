39aa58abc2fed24e76f883e507fdef2d6bea1210
Ciel806 <ceryne688@gmail.com>
2026-03-16 13:36:28 +0700

    week07: implemment companion object as factory in NetworkClient

--- src/oop_00000135652_Ceryne/week07/NetworkClient.kt
+++ src/oop_00000135652_Ceryne/week07/NetworkClient.kt
@@ -1,6 +1,15 @@
 package oop_00000135652_Ceryne.week07
 
 class NetworkClient private constructor(val url: String) {
+    companion object {
+        const val BASE_URL = "https://api.umn.ac.id" //shared content
+
+        fun createClient(): NetworkClient {
+            println("Membangun NetworkClient dengan BASE_LINE: $BASE_URL")
+            return NetworkClient(BASE_URL)
+        }
+    }
+
     fun connect() {
         println("Connecting to $url...")
     }
