e438afb349e765897db64e6249bcb8b29c0c85b6
Ciel806 <ceryne688@gmail.com>
2026-04-13 14:13:02 +0700

    week08: implement safe call with let block

--- src/oop_00000135652_Ceryne/week08/Main.kt
+++ src/oop_00000135652_Ceryne/week08/Main.kt
@@ -7,4 +7,15 @@ fun Main() {
     //rantai safe calls yang elegan
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+
+    val receipt = validOrder.totalPrice?.let { price ->
+        //blok ini HANYA berjalan jika total price tidak null
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println(receipt)
 }
