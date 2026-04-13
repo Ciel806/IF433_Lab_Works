cffd078d8e5fb39df58410504756612c6d4acabd
Ciel806 <ceryne688@gmail.com>
2026-04-13 15:39:26 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000135652_Ceryne/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000135652_Ceryne.week08;
+
+public class LegacyJavaAPI {
+    //fungsi ini ditulis dalam java. kotlin tidak tahu apakah kembaliannya nullable
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
