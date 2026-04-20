ddacc981c6941e9006a7a17ca752557e4b271874
Ciel806 <ceryne688@gmail.com>
2026-04-20 19:26:00 +0700

    week09: (task) chain sortedBy and map for worst performers

--- src/oop_00000135652_Ceryne/week09/task/TradingDashboard.kt
+++ src/oop_00000135652_Ceryne/week09/task/TradingDashboard.kt
@@ -6,7 +6,7 @@ fun main() {
         TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
         TradeLog("SOLUSDT", "LONG", 5, 25.0, "OPEN"),
         TradeLog("BTCUSDT", "SHORT", 15, -12.0, "CLOSED"),
-        TradeLog("ETHUSDT", "LONG", 10, 8.4, "OPEN"),
+        TradeLog("ETHUSDT", "LONG", 10, 8.4, "CLOSED"),
         TradeLog("ADAUSDT", "LONG", 2, -2.0, "OPEN")
     )
 
@@ -16,5 +16,9 @@ fun main() {
     val topPerformerString = winningTrades
         .sortedByDescending { it.roe }
         .map { println("WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)") }
+
+    val worstPerformersString = losingTrades
+        .sortedBy { it.roe }
+        .map { println("LOSS [${it.pair} - ${it.position}]: -${it.leverage}% (Lev: ${it.leverage})") }
 
 }
