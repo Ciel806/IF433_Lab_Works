2d2f2e779c20e4917387b49455e941a595849554
Ciel806 <ceryne688@gmail.com>
2026-04-27 22:54:05 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000135652_Ceryne/week10/task/CryptoDashboard.kt
+++ src/oop_00000135652_Ceryne/week10/task/CryptoDashboard.kt
@@ -15,4 +15,9 @@ fun main() {
     response.data.forEach { coin ->
         println("Coin ${coin.name}, Balance: ${coin.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+
+    txRepo.add(Transaction("TX001", 0.1))
+    txRepo.add(Transaction("TX002", 50.5))
 }
