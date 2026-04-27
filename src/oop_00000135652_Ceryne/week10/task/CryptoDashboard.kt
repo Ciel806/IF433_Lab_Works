f6b2ce472ea133b7734a84889470983dc58e3006
Ciel806 <ceryne688@gmail.com>
2026-04-27 22:46:10 +0700

    week10: (task) populate coin repository

--- src/oop_00000135652_Ceryne/week10/task/CryptoDashboard.kt
+++ src/oop_00000135652_Ceryne/week10/task/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_00000135652_Ceryne.week10.task
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    //isi data
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 1.2))
+    coinRepo.add(Coin("USDT", 100.0))
 }
