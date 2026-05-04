583c7343acb7fc6d6e42efc98695abb2d729c69f
Ciel806 <ceryne688@gmail.com>
2026-05-04 20:53:30 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000135652_Ceryne/week11/task/SmartHomeDashboard.kt
+++ src/oop_00000135652_Ceryne/week11/task/SmartHomeDashboard.kt
@@ -2,4 +2,17 @@ package oop_00000135652_Ceryne.week11.task
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+
+    println("=== Konfigurasi Pencahayaan (apply & also) ===")
+    val lampu = SmartDevice().apply {
+        name = "Philips WiZ Living Room"
+        category = "Lighting"
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        println("Name: ${it.name}\nCategory: ${it.category}\n" +
+                "isOnline: ${it.isOnline}\nPowerLoad: ${it.powerLoad}")
+        homeDevices.add(it)
+    }
+
 }
--- src/oop_00000135652_Ceryne/week11/task/SmartHomeModels.kt
+++ src/oop_00000135652_Ceryne/week11/task/SmartHomeModels.kt
@@ -1,8 +1,8 @@
 package oop_00000135652_Ceryne.week11.task
 
 data class SmartDevice(
-    var name: String,
-    var category: String,
+    var name: String = "",
+    var category: String = "",
     var isOnline: Boolean = false,
     var powerLoad: Int = 0
 )
