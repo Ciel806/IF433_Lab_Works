77c581c8b0e746f58533812de927d88a2a482c7b
Ciel806 <ceryne688@gmail.com>
2026-05-04 21:02:12 +0700

    week11: (task) configure AC unit using run

--- src/oop_00000135652_Ceryne/week11/task/SmartHomeDashboard.kt
+++ src/oop_00000135652_Ceryne/week11/task/SmartHomeDashboard.kt
@@ -24,4 +24,13 @@ fun main() {
         homeDevices.add(it)
     }
 
+    println("\n=== Konfigurasi AC & Kabel (run) ===")
+    val ac = run{
+        SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
+    }
+    homeDevices.add(ac)
+
+    val feeder = SmartDevice("Picolo's Auto Feeder","Pet Care", true, 10)
+    homeDevices.add(feeder)
+
 }
