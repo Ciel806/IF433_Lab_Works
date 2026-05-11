127acbbeebfdbcc09a58c0d9b7cf072ed0980e8d
Ciel806 <ceryne688@gmail.com>
2026-05-11 14:38:50 +0700

    week 12: (task) implement hardware validation with throw

--- src/oop_00000135652_Ceryne/week12/task/SmartFeeder.kt
+++ src/oop_00000135652_Ceryne/week12/task/SmartFeeder.kt
@@ -4,4 +4,8 @@ fun dispenseKibble(requestGram: Int, availableGram: Int, isJammed: Boolean): Int
     require(requestGram > 0) {
         "Porsi kibble harus lebih dari 0 gr"
     }
+
+    if (isJammed) {
+        throw DispenserJamException()
+    }
 }
