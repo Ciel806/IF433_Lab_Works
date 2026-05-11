916866f3bb0e0ecfbb1c1f826fd9fe2e9e8cea90
Ciel806 <ceryne688@gmail.com>
2026-05-11 14:42:06 +0700

    week 12: (task) implement food stock validation with throw

--- src/oop_00000135652_Ceryne/week12/task/SmartFeeder.kt
+++ src/oop_00000135652_Ceryne/week12/task/SmartFeeder.kt
@@ -1,11 +1,18 @@
 package oop_00000135652_Ceryne.week12.task
 
-fun dispenseKibble(requestGram: Int, availableGram: Int, isJammed: Boolean): Int {
-    require(requestGram > 0) {
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) {
         "Porsi kibble harus lebih dari 0 gr"
     }
 
     if (isJammed) {
         throw DispenserJamException()
     }
+
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, requestedGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
 }
