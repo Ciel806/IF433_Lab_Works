5f678d83aac3af598ed3adc81cb710630e3b7621
Ciel806 <ceryne688@gmail.com>
2026-05-18 15:11:39 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000135652_Ceryne/week13/task/CryptoTradeLogger.kt
@@ -0,0 +1,5 @@
+package oop_00000135652_Ceryne.week13.task
+
+data class TradeRecord (val id: Int, val symbol: String,
+                        val type: String, val margin: Double,
+                        val pnl: Double)
